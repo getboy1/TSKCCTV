6bee7d587292b0e9c759bcb91eaf2c544d172d3a
boy <boy_ob039@gmail.com>
2018-04-01 22:45:56 +0700

    up flie 14

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,8 @@ Route::post('/admin-dashboard','AdminController@dashboard');
 
 //Category related routes
 Route::get('/add-category','CategoryController@index');
+Route::get('/all-category','CategoryController@all_category');
+Route::post('/save-category','CategoryController@save_category');
+Route::get('/edit-category/{category_id}','CategoryController@edit_category');
+Route::get('/unactive_category/{category_id}','CategoryController@unactive_category');
+Route::get('/active_category/{category_id}','CategoryController@active_category');
